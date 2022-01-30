b2c635d5c1c2ebf927bbf53255c65258f3482540
Jefferson Ye <forbesye@users.noreply.github.com>
2022-01-29 21:10:31 -0600

    cant type correctly

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -26,7 +26,7 @@ const SEO = (props) => {
 
   return (
     <Helmet title={seo.title} titleTemplate={titleTemplate}>
-      <meta name="theme-color" content="#0F172A /">
+      <meta name="theme-color" content="#0F172A" />
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
       {seo.url && <meta property="og:url" content={seo.url} />}
