f521ef626e91f1bdf2c660649419877d9634c8a3
Jefferson Ye <forbesye@users.noreply.github.com>
2022-01-29 19:36:38 -0600

    added map

---
+++ src/components/map.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import USMap from "../assets/states-10m.json";
+import AT from "../assets/at.json";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+  Line,
+  Marker,
+  Annotation,
+} from "react-simple-maps";
+
+const MapChart = () => {
+  const [zoom, setZoom] = useState<number>(2);
+
+  return (
+    <div className="my-4 border-4 border-white relative">
+      <ComposableMap projection="geoAlbers">
+        <ZoomableGroup
+          zoom={zoom}
+          center={[-77.2, 40.7]}
+          minZoom={1}
+          maxZoom={30}
+        >
+          <>
+            <Geographies geography={USMap}>
+              {({ geographies }) => (
+                <>
+                  {geographies.map((geo, idx) => (
+                    <Geography
+                      className="rounded"
+                      key={idx}
+                      stroke="#FFF"
+                      strokeWidth={0.5}
+                      strokeLinecap="round"
+                      geography={geo}
+                      fill="#CBD5E1"
+                    />
+                  ))}
+                </>
+              )}
+            </Geographies>
+            {AT.coordinates.map((curr, idx, arr) => {
+              if (idx !== arr.length - 1) {
+                return (
+                  <Line
+                    key={idx}
+                    to={arr[idx + 1]}
+                    from={curr}
+                    stroke="#EF4444"
+                    strokeWidth={0.5}
+                    strokeLinecap="round"
+                  />
+                );
+              }
+              return null;
+            })}
+          </>
+        </ZoomableGroup>
+      </ComposableMap>
+      <div className="absolute bottom-6 right-6">
+        <div className="flex flex-col">
+          <button
+            className="bg-white py-2 px-4 mb-3 text-slate-900 rounded-full drop-shadow-xl"
+            onClick={() => setZoom(zoom + 1)}
+          >
+            +
+          </button>
+          <button
+            className="bg-white py-2 px-4 text-slate-900 rounded-full drop-shadow-xl"
+            onClick={() => setZoom(Math.max(zoom - 1, 1))}
+          >
+            -
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MapChart;
