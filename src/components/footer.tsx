99e9e9b4c7e836735cb6ea5b7011cd72fe259660
Jefferson Ye <forbesye@users.noreply.github.com>
2022-01-30 21:37:09 -0600

    footer font size change

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="flex flex-col text-base text-center mb-12 md:block">
+    <footer className="flex flex-col text-lg text-center mb-12 md:block">
       <span>© {new Date().getFullYear()} Jefferson Ye</span>
       <span className="m-0 p-0 border-0 md:ml-5 md:pl-5 md:border-l">
         <a href="https://www.linkedin.com/in/jefferson-ye/">LinkedIn</a>
