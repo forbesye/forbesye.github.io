00546e404afbfdb32d7466177af16c15dd72b756
Jefferson Ye <forbesye@users.noreply.github.com>
2022-02-06 13:50:46 -0600

    loadable refactor

--- src/components/lazyMap.tsx
+++ src/components/lazyMap.tsx
@@ -3,10 +3,4 @@ import loadable from "@loadable/component";
 
 const LazyMap: React.FC = loadable(() => import("./blogMap"));
 
-const Wrapper: React.FC = () => (
-  <>
-    <LazyMap />
-  </>
-);
-
-export default Wrapper;
+export default LazyMap;
